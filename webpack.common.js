dcd52a64aae4a94e543ee5736670a48811e636f1
Przemysław Wysocki <przewysocki@gmail.com>
2020-11-16 18:47:15 +0100

    initial commit

--- webpack.common.js
+++ webpack.common.js
@@ -88,5 +88,11 @@ module.exports = {
       chunks: ["index", "another"],
       filename: "another.html",
     }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/test.html",
+      inject: true,
+      chunks: ["index", "another"],
+      filename: "test.html",
+    }),
   ],
 };
